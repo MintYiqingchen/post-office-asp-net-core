d83b5938bf6b745c130d6f088e976991dca31a23
unknown <821244608@qq.com>
2017-12-31 23:43:47 +0800

    move from old project

--- PostOfficeApp/Controllers/HomeController.cs
+++ PostOfficeApp/Controllers/HomeController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using PostOfficeApp.Models;
 
 namespace PostOfficeApp.Controllers
@@ -32,6 +34,37 @@ namespace PostOfficeApp.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [Route("/Home/search")]
+        public JsonResult Search(string q)
+        {
+            JObject returnContent = new JObject();
+            IEnumerable<string> keys = q.ToLower().Split();
+            foreach (var s in keys)
+            {
+                if (Regex.IsMatch(s, @"^[0-9]+-[0-9]+$"))
+                {
+                    returnContent = new JObject(
+                        new JProperty("results",
+                            new JArray(
+                                new JObject(new JProperty("title", "test title"), new JProperty("url", "/test"))
+                            )
+                        ));
+                    return new JsonResult(returnContent);
+                }
+                else
+                {
+                    /* todo :search in database*/
+                    returnContent = new JObject(
+                        new JProperty("results",
+                            new JArray(
+                                new JObject(new JProperty("title", "test title"), new JProperty("url", "/test"))
+                            )
+                        ));
+                    break;
+                }
+            }
+            return new JsonResult(returnContent);
         }
     }
 }
---
+++ PostOfficeApp/wwwroot/js/topnav.js
@@ -0,0 +1,21 @@
+﻿// Write your JavaScript code.
+'use strict'
+
+$(document).ready(function () {
+    $(".ui.dropdown").dropdown({ on: "hover" });
+    $.fn.search.settings.error.noResults = '搜索不到结果';
+    $.fn.search.settings.onSearchQuery = searchKey;
+    $(".ui.search").search({
+        apiSettings: { url: '/Home/search/?q={query}' },
+        minCharacters:3
+    });
+
+});
+
+function searchKey(element, searchTerm) {
+    $(document).keyup(function (event) {
+        if (event.keyCode == 13) {
+            $(location).attr("href", "/Test?q="+searchTerm);
+        }
+    });
+}
